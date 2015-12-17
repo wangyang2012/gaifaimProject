9091562e6de278ec4192c0aaf16e9df2715fb6c6
wangyang1712 <wangyang1712@gmail.com>
2015-12-17 01:05:38 +0100

    sql script

--- src/main/resources/gaifaim.sql
+++ src/main/resources/gaifaim.sql
@@ -1,10 +1,8 @@
-CREATE DATABASE  IF NOT EXISTS `gaifaim` /*!40100 DEFAULT CHARACTER SET utf8 */;
-USE `gaifaim`;
--- MySQL dump 10.13  Distrib 5.6.13, for Win32 (x86)
+-- MySQL dump 10.15  Distrib 10.0.17-MariaDB, for Win32 (AMD64)
 --
--- Host: 127.0.0.1    Database: gaifaim
+-- Host: localhost    Database: gaifaim
 -- ------------------------------------------------------
--- Server version	5.6.15-log
+-- Server version	10.0.17-MariaDB
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -18,6 +16,161 @@ USE `gaifaim`;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
+-- Table structure for table `jour_has_menu`
+--
+
+DROP TABLE IF EXISTS `jour_has_menu`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `jour_has_menu` (
+  `jour` datetime NOT NULL,
+  `id_menu` int(11) NOT NULL,
+  PRIMARY KEY (`jour`,`id_menu`),
+  KEY `fk_menu_jour_idx` (`id_menu`),
+  CONSTRAINT `fk_menu_jour` FOREIGN KEY (`id_menu`) REFERENCES `menu` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `jour_has_menu`
+--
+
+LOCK TABLES `jour_has_menu` WRITE;
+/*!40000 ALTER TABLE `jour_has_menu` DISABLE KEYS */;
+/*!40000 ALTER TABLE `jour_has_menu` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `menu`
+--
+
+DROP TABLE IF EXISTS `menu`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `menu` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `description` varchar(255) DEFAULT NULL,
+  `titre` varchar(45) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `id_UNIQUE` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `menu`
+--
+
+LOCK TABLES `menu` WRITE;
+/*!40000 ALTER TABLE `menu` DISABLE KEYS */;
+/*!40000 ALTER TABLE `menu` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `menu_has_photo`
+--
+
+DROP TABLE IF EXISTS `menu_has_photo`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `menu_has_photo` (
+  `id_menu` int(11) NOT NULL,
+  `id_photo` int(11) NOT NULL,
+  PRIMARY KEY (`id_menu`,`id_photo`),
+  KEY `fk_id_photo_idx` (`id_photo`),
+  CONSTRAINT `fk_id_menu` FOREIGN KEY (`id_menu`) REFERENCES `menu` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `fk_id_photo` FOREIGN KEY (`id_photo`) REFERENCES `photo` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `menu_has_photo`
+--
+
+LOCK TABLES `menu_has_photo` WRITE;
+/*!40000 ALTER TABLE `menu_has_photo` DISABLE KEYS */;
+/*!40000 ALTER TABLE `menu_has_photo` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `photo`
+--
+
+DROP TABLE IF EXISTS `photo`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `photo` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `filename` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `id_UNIQUE` (`id`),
+  UNIQUE KEY `filename_UNIQUE` (`filename`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `photo`
+--
+
+LOCK TABLES `photo` WRITE;
+/*!40000 ALTER TABLE `photo` DISABLE KEYS */;
+/*!40000 ALTER TABLE `photo` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `reservation`
+--
+
+DROP TABLE IF EXISTS `reservation`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `reservation` (
+  `date` datetime NOT NULL,
+  `id_utilisateur` int(11) NOT NULL,
+  `id_menu` int(11) NOT NULL,
+  `quantite` int(11) NOT NULL,
+  PRIMARY KEY (`date`,`id_utilisateur`,`id_menu`),
+  KEY `fk_id_utilisateur_menu_idx` (`id_utilisateur`),
+  KEY `fk_id_menu_utilisateur_idx` (`id_menu`),
+  CONSTRAINT `fk_id_menu_utilisateur` FOREIGN KEY (`id_menu`) REFERENCES `menu` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `fk_id_utilisateur_menu` FOREIGN KEY (`id_utilisateur`) REFERENCES `utilisateur` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `reservation`
+--
+
+LOCK TABLES `reservation` WRITE;
+/*!40000 ALTER TABLE `reservation` DISABLE KEYS */;
+/*!40000 ALTER TABLE `reservation` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `role`
+--
+
+DROP TABLE IF EXISTS `role`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `role` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `role` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `id_UNIQUE` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `role`
+--
+
+LOCK TABLES `role` WRITE;
+/*!40000 ALTER TABLE `role` DISABLE KEYS */;
+INSERT INTO `role` VALUES (1,'ADMIN'),(2,'USER');
+/*!40000 ALTER TABLE `role` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Table structure for table `utilisateur`
 --
 
@@ -28,14 +181,10 @@ CREATE TABLE `utilisateur` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `login` varchar(45) NOT NULL,
   `mdp` blob NOT NULL,
-  `id_role` int(11) NOT NULL,
+  `email` varchar(100) DEFAULT NULL,
   `telephone` varchar(45) NOT NULL,
-  `email` varchar(255) NOT NULL,
-  `nom` varchar(45) NOT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `id_UNIQUE` (`id`),
-  UNIQUE KEY `login_UNIQUE` (`login`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -44,8 +193,35 @@ CREATE TABLE `utilisateur` (
 
 LOCK TABLES `utilisateur` WRITE;
 /*!40000 ALTER TABLE `utilisateur` DISABLE KEYS */;
-INSERT INTO `utilisateur` VALUES (1,'admin','admin',1,'0612345678','admin@gaifaim.com','ADMIN');
+INSERT INTO `utilisateur` VALUES (1,'admin','admin','admin@gaifaim.com','0612345678'),(2,'yang','thomas','yang@gaimfaim.com','0612345678'),(3,'test','utilisateur','utilisateur@gaifaim.com','012345678');
 /*!40000 ALTER TABLE `utilisateur` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `utilisateur_has_role`
+--
+
+DROP TABLE IF EXISTS `utilisateur_has_role`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `utilisateur_has_role` (
+  `id_utilisateur` int(11) NOT NULL,
+  `id_role` int(11) NOT NULL,
+  PRIMARY KEY (`id_utilisateur`,`id_role`),
+  KEY `fk_id_role_idx` (`id_role`),
+  CONSTRAINT `fk_id_role` FOREIGN KEY (`id_role`) REFERENCES `role` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `fk_id_utilisateur` FOREIGN KEY (`id_utilisateur`) REFERENCES `utilisateur` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `utilisateur_has_role`
+--
+
+LOCK TABLES `utilisateur_has_role` WRITE;
+/*!40000 ALTER TABLE `utilisateur_has_role` DISABLE KEYS */;
+INSERT INTO `utilisateur_has_role` VALUES (1,1),(1,2),(2,1),(2,2);
+/*!40000 ALTER TABLE `utilisateur_has_role` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
@@ -57,4 +233,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2015-12-14 16:55:13
+-- Dump completed on 2015-12-17  1:02:06
